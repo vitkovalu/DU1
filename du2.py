81b880858f300db3a8516904365847e3513f96df
Ludmila Vítková <vitkovalu@natur.cuni.cz>
2022-12-05 14:45:42 +0100

    kazdy_sedmy

--- du2.py
+++ du2.py
@@ -4,10 +4,13 @@ with open ("QD_138000_Data.csv", encoding="utf-8", newline='') as f:
     reader = csv.reader(f, delimiter=",")
 
     with open("vystup_7dni.csv","w",encoding="utf-8", newline='') as g:  #zalozen soubor pro sedmidenní prumery
-        for row in reader:
-            for index, item in enumerate (reader, start = 1):
-                g.write(f"{index}:  {row[-2]} {row[-1]} \n" )
-
+        for index, row in enumerate (reader):
+            cislo_radku= int(index)
+            if cislo_radku%7 == 0:       #vypíše jen každý sedmy (začíná nulou), pokud je delitelné 7 beze zbytku
+                g.write(str(index) + str(row) +"\n" )
+            
     with open("vystup_rok.csv","w",encoding="utf-8", newline='') as h:  #zalozen soubor pro roční průměry
-        for row in reader:
-            h.write(row[-2] + " " + row[-1] + "\n")
+        for index, row in enumerate (reader):
+            cislo_radku = int(index)
+            if cislo_radku%365 == 0:
+                h.write(str(index) + str(row) +"\n" )
