3d747530837415f251357aa1736a69e847c9696b
Ludmila Vítková <vitkovalu@natur.cuni.cz>
2022-12-05 09:20:47 +0100

    zalozeni souboru

--- du2.py
+++ du2.py
@@ -1,1 +1,11 @@
-#ukol2
+import csv
+
+with open ("QD_138000_Data.csv", encoding="utf-8", newline='') as f:
+    with open("vystup_7dni.csv","w",encoding="utf-8", newline='') as g:  #zalozen soubor pro sedmidenní prumery
+        reader = csv.reader(f, delimiter=",")
+        for row in reader:
+            g.write(row[-2] + " " + row[-1] + "\n")
+    with open("vystup_rok.csv","w",encoding="utf-8", newline='') as h:  #zalozen soubor pro roční průměry
+        reader = csv.reader(f, delimiter=",")
+        for row in reader:
+            h.write(row[-2] + " " + row[-1] + "\n")
