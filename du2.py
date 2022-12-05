54b46a8ba125c075c1b8c4e29303231c037462c9
Ludmila Vítková <vitkovalu@natur.cuni.cz>
2022-12-05 14:53:51 +0100

    zmena_open

--- du2.py
+++ du2.py
@@ -1,16 +1,13 @@
 import csv
 
-with open ("QD_138000_Data.csv", encoding="utf-8", newline='') as f:
+with open ("QD_138000_Data.csv", encoding="utf-8", newline='') as f,\
+    open("vystup_7dni.csv","w",encoding="utf-8", newline='') as g, \
+    open("vystup_rok.csv","w",encoding="utf-8", newline='') as h:
     reader = csv.reader(f, delimiter=",")
+    for index, row in enumerate (reader):
+        cislo_radku= int(index)
+        if cislo_radku%7 == 0:       #vypíše jen každý sedmy (začíná nulou), pokud je delitelné 7 beze zbytku
+            g.write(str(index) + str(row) +"\n" )
 
-    with open("vystup_7dni.csv","w",encoding="utf-8", newline='') as g:  #zalozen soubor pro sedmidenní prumery
-        for index, row in enumerate (reader):
-            cislo_radku= int(index)
-            if cislo_radku%7 == 0:       #vypíše jen každý sedmy (začíná nulou), pokud je delitelné 7 beze zbytku
-                g.write(str(index) + str(row) +"\n" )
-            
-    with open("vystup_rok.csv","w",encoding="utf-8", newline='') as h:  #zalozen soubor pro roční průměry
-        for index, row in enumerate (reader):
-            cislo_radku = int(index)
-            if cislo_radku%365 == 0:
-                h.write(str(index) + str(row) +"\n" )
+        if cislo_radku%365 == 0:
+            h.write(str(index) + str(row) +"\n" )
