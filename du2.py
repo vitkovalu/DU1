dab289706208efb5fe3db4267cd5670e5df0e3f9
Ludmila Vítková <vitkovalu@natur.cuni.cz>
2022-12-05 12:10:24 +0100

    enumerate

--- du2.py
+++ du2.py
@@ -1,11 +1,13 @@
 import csv
 
 with open ("QD_138000_Data.csv", encoding="utf-8", newline='') as f:
+    reader = csv.reader(f, delimiter=",")
+
     with open("vystup_7dni.csv","w",encoding="utf-8", newline='') as g:  #zalozen soubor pro sedmidenní prumery
-        reader = csv.reader(f, delimiter=",")
         for row in reader:
-            g.write(row[-2] + " " + row[-1] + "\n")
+            for index, item in enumerate (reader, start = 1):
+                g.write(f"{index}:  {row[-2]} {row[-1]} \n" )
+
     with open("vystup_rok.csv","w",encoding="utf-8", newline='') as h:  #zalozen soubor pro roční průměry
-        reader = csv.reader(f, delimiter=",")
         for row in reader:
             h.write(row[-2] + " " + row[-1] + "\n")
