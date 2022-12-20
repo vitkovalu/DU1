892c0575cf0b5100781411e30c1349f7b57e6119
Ludmila Vítková <vitkovalu@natur.cuni.cz>
2022-12-20 19:44:19 +0100

    nahran json soubory

--- ukol3/du3.py
+++ ukol3/du3.py
@@ -1,1 +1,8 @@
 #ukol3
+import json
+
+with open("adresy.geojson", encoding ="utf-8") as f:
+    data = json.load(f)
+
+with open("kontejnery.json", encoding ="utf-8") as g:
+    data = json.load(g)
