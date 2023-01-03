ce3e8fd45224e7c8110fbd8dc5a914e1a944aa5f
Ludmila Vítková <vitkovalu@natur.cuni.cz>
2023-01-03 13:03:57 +0100

    FileNotFoundError

--- ukol3/du3.py
+++ ukol3/du3.py
@@ -3,8 +3,16 @@ from pyproj import Transformer
 
 wgs2jtsk = Transformer.from_crs(4326,5514,always_xy=True)
 
-with open("adresy.geojson", encoding ="utf-8") as f:
-    data = json.load(f)
+try:
+    with open("adresy.geojson", encoding ="utf-8") as adresy:
+        data_adresy = json.load(adresy)
+except FileNotFoundError:
+    print("Soubor neexistuje")
+try:
+    with open("kontejnery.json", encoding ="utf-8") as kontejnery:
+        data_kontejnery = json.load(kontejnery)
+except FileNotFoundError:
+    print("Soubor neexistuje")
 
-with open("kontejnery.json", encoding ="utf-8") as g:
-    data = json.load(g)
+
+
