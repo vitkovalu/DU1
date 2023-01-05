089b06e1f54e07788fafb013fe256c84c3a61408
Ludmila Vítková <vitkovalu@natur.cuni.cz>
2023-01-05 10:56:08 +0100

    vzdalenost nad 10 km, max vzdalenost nově, median

--- ukol3/du3.py
+++ ukol3/du3.py
@@ -1,23 +1,27 @@
 import json
 from pyproj import Transformer
 from statistics import mean, median
-from math import sqrt, inf
+from math import sqrt
+from json.decoder import JSONDecodeError
 
 wgs2jtsk = Transformer.from_crs(4326,5514,always_xy=True)   #prevod na krovaka
-min_vzdalenost = inf
-max_vzdalenost=0
+min_vzdalenost = None
 
 try:
     with open("adresy.geojson", encoding ="utf-8") as adresy:    #nactení adres
         data_adresy = json.load(adresy)
 except FileNotFoundError:
-    print("Soubor neexistuje")
+    print("Vstupní soubor s adresami neexistuje")
+except JSONDecodeError:
+    print("Vstupní soubor s adresami není validní")
 
 try:
     with open("kontejnery.json", encoding ="utf-8") as kontejnery: #nactení kontejnerů
         data_kontejnery = json.load(kontejnery)
 except FileNotFoundError:
-    print("Soubor neexistuje")
+    print("Vstupní soubor s kontejnery neexistuje")
+except JSONDecodeError:
+    print("Vstupní soubor s kontejnery není validní")
 
 pocet_adres = len(data_adresy["features"])     
 pocet_kontejnery = len(data_kontejnery["features"])    #spocten celkový pocet adres a kontejnerů
@@ -41,17 +45,28 @@ try:
                 
                 if min_vzdalenost == None or min_vzdalenost>vzdalenost:
                     min_vzdalenost = vzdalenost
-                    nejbliz = aktualni_kontejner
-            if min_vzdalenost > max_vzdalenost:
-                max_vzdalenost = min_vzdalenost
-                nejdal = aktualni_kontejner
+                    nejbliz_kontejner = aktualni_kontejner
+
+        if min_vzdalenost > 10000:
+            print("Nejbližší kontejner se nachází dále než 10 km")
+            exit()             #program se ukončí, je-li nejbližší program vzdálenn dál než 10 km
 
         adresa["properties"]["vzdalenost_od_kont"] = min_vzdalenost
-        adresa["properties"]["kontejner"] = nejbliz        
-        min_vzdalenost = 0   #opět se vynuluje
+        adresa["properties"]["kontejner"] = nejbliz_kontejner        
+        min_vzdalenost = None  #opět se vynuluje
 
 except KeyError:
-    print("neco je spatne")
+    print("Soubor nemá všechny požadované atributy")
 
+vzdalenosti = [adresa["properties"]["vzdalenost_od_kont"] for adresa in data_adresy["features"]]
+                                          
+max_vzdalenost = round(max(vzdalenosti))
+index_kontejneru= (vzdalenosti.index(max(vzdalenosti)))   #najde index maximální vzdálenosti, čímž se uloží/najde adresa toho místa
+ulice = data_adresy["features"][index_kontejneru]["properties"]["addr:street"]
+cislo_domu = data_adresy["features"][index_kontejneru]["properties"]["addr:housenumber"]
+
+print(f"Průměrná vzdálenost ke kontejneru je {round(mean(vzdalenosti),1)} m")            
+print(f"Medián vzdálenosti ke kontejneru: {round(median(vzdalenosti),1)} m")               
+print(f"Nejdále ke kontejneru je z adresy {ulice} {cislo_domu} a to {max_vzdalenost} m")
 
 
